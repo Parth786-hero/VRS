17e5b34ffaaff5183662900b88b116747db88d01
SKAUR28 <skaur28@jaguarlandrover.com>
2025-09-08 14:00:35 +0100

    structure

---
+++ Register-Vehicle/src/main/java/com/vrs/Register_Vehicle/RegisterVehicleApplication.java
@@ -0,0 +1,13 @@
+package com.vrs.Register_Vehicle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterVehicleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterVehicleApplication.class, args);
+	}
+
+}
---
+++ Register-Vehicle/src/test/java/com/vrs/Register_Vehicle/RegisterVehicleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrs.Register_Vehicle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterVehicleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
